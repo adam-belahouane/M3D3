bbae3da41335eae614bdf48b5c9f9c2966b16f12
adam-belahouane <adam_belahouane@outlook.com>
2021-09-15 15:55:32 +0100

    changes

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const doingSomthing = () => {
-    api()
-        .then((response) => {
-            return response.json()
+// const doingSomthing = () => {
+//     api()
+//         .then((response) => {
+//             return response.json()
 
-        })
-        .then((body) => {
-            console.log(body)
-        })
-}
+//         })
+//         .then((body) => {
+//             console.log(body)
 
-const api = () => {
-    return fetch('https://api.pexels.com/videos/search?query=People', {
-        "method": "GET",
-        "headers": {
-            "Authorization": "563492ad6f9170000100000120628be3036d41feb0b2936d48c5d3a4"
-        }
-    })
-}
+//         })
+// }
+
+// const api = () => {
+//     return fetch('https://api.pexels.com/videos/search?query=People', {
+//         "method": "GET",
+//         "headers": {
+//             "Authorization": "563492ad6f9170000100000120628be3036d41feb0b2936d48c5d3a4"
+//         }
+//     })
+// }
 
 
-window.onload = function () {
-    doingSomthing()
-}
+// window.onload = function () {
+//     doingSomthing()
+// }
