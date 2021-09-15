95faf22d7035eeb874090a36d3159c19e0eeadff
Zee <xziyad92@gmail.com>
2021-09-15 20:59:21 +0200

    sa

--- index.js
+++ index.js
@@ -43,7 +43,48 @@ const searchImg = () => {
             console.log('An Error')
         })
 }
+const seconderyBtn = () => {
+    let secondry = document.querySelector('#secondary-img')
 
+    secondry.addEventListener('click', () => {
+        api('surf')
+        // console.log(api('surf').then(data => data.json()))
+    })
+}
+
+const createSecond = (body) => {
+    seconderyBtn()
+
+    let row = document.querySelector('#card-row')
+    row.innerHTML = ''
+    body.photos.forEach((photo) => {
+        let img = photo
+        let col = document.createElement('div')
+        col.className = 'col-md-4'
+        col.innerHTML = `<div class="card mb-4 shadow-sm">
+                              <img src="${img.src.medium}" alt="">
+                              <div class="card-body">
+                                <p class="card-text">
+                                  This is a wider card with supporting text below as a natural
+                                  lead-in to additional content. This content is a little bit
+                                  longer.
+                                </p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                  <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">
+                                      View
+                                    </button>
+                                    <button onclick="hide(this.closest('.col-md-4'))" type="button" class="btn btn-sm btn-outline-secondary">
+                                      Hide
+                                    </button>
+                                  </div>
+                                  <small class="text-muted">9 mins</small>
+                                </div>
+                              </div>
+                            </div>`
+        row.appendChild(col)
+    })
+}
 const api = (search) => {
     // return fetch('https://api.pexels.com/v1/search?query=' + search, {
     return fetch('https://api.pexels.com/v1/search?query=' + search, {
@@ -61,6 +102,6 @@ const hide = (e) => {
     // ".closest"
 }
 
-window.onload = function () {
-    // searchImg()
+window.onload = () => {
+    seconderyBtn()
 }
